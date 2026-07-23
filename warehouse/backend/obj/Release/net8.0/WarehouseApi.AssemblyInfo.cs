02b5eb0701eebee267fb14bcc7583cbc309d818e
rudyrianda <meriandini654@gmail.com>
2026-07-23 08:51:44 +0700

    Update from local workspace

--- warehouse/backend/obj/Release/net8.0/WarehouseApi.AssemblyInfo.cs
+++ warehouse/backend/obj/Release/net8.0/WarehouseApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WarehouseApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8861332654790ef777efea313bf8e33872e1a1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WarehouseApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WarehouseApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
